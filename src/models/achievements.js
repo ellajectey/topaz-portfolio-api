ac0f54ef6d8b16188bda8103591bdcb24ea00774
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-03-27 23:42:41 +0000

    added title to achievement model

--- src/models/achievements.js
+++ src/models/achievements.js
@@ -6,8 +6,9 @@ const schema = mongoose.Schema
 const AchievementSchema =new schema ({
 
     image: {type: String,default: null},
+    title: {type: String,required: true},
     date: {type: Date,required: true},
-    description: {type: String,required: true},
+    description: {type: String,default: null},
     createdAt: {type: Date, default: Date.now}
 
 
