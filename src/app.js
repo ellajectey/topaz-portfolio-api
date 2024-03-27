c43592cd8521a61b42660f45352ee9908c78ae48
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-03-27 17:40:37 +0000

    swagger update

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import {router} from './routes/allroutes.js'
+import { handleRequests, init, handleResponses } from "express-oas-generator";
 
 
 dotenv.config();
 
+const modelNames = mongoose.modelNames();
+
 const app = express();
+handleResponses(app);
+
 
 app.use(express.json());
 
@@ -27,7 +32,36 @@ try {
 const port = process.env.PORT || 8080
 
 app.listen(port, () =>{
+    init(
+        app,
+        (spec) => {
+          spec.info = {
+            title: "Topaz Portfolio API Documentation",
+            description: "API Documentation for Topaz Portfolio website",
+          };
+          spec.host = "topaz-portfolio-api.onrender.com";
+          spec.schemes = ["http", "https"];
+    
+          
+          return spec;
+        },
+        "/swagger.json",
+        60 * 1000,
+        "api-docs",
+        modelNames,
+        ["users"],
+        ["production"],
+        true
+      );
+    
+      console.log(
+        'Server Listening on  8000, Open http://localhost:8000/api-docs/'
+      );
+    });
+    
+    app.use(router);
 
     console.log(`Express server is running on ${port}`);
- 
-});
+
+     
+    handleRequests();
